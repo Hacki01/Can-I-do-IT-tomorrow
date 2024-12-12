a6fdaf69d004540e8d3b4d0f5d2daacb0bc12838
Hacki <69818162+Hacki01@users.noreply.github.com>
2024-12-12 19:33:33 +0100

    localstorageLoader dateTimeReviver types added

--- src/TasksStore/localstorageLoader.tsx
+++ src/TasksStore/localstorageLoader.tsx
@@ -5,7 +5,7 @@ import { useDispatch } from "react-redux";;
 import { setTasks } from "./Features/tasks/taskManager";
 import type { TasksSlice } from "./Features/tasks/taskManager";
 
-function dateTimeReviver(key, value) {
+function dateTimeReviver(key: string, value:string) {
   // Matches strings like "2022-08-25T09:39:19.288Z"
   const isoDateRegex = /^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{3}Z$/
 
