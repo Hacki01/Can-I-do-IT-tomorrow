f457ef42f194faf825b05e3c8b8d018ab3921efc
Hacki <69818162+Hacki01@users.noreply.github.com>
2024-12-14 20:16:11 +0100

    task add button to planned date

--- src/components/panel/index.tsx
+++ src/components/panel/index.tsx
@@ -1,9 +1,17 @@
 import { useDispatch } from 'react-redux'
 import { addTask } from "@/TasksStore/Features/tasks/taskManager";
 import {Input, Textarea} from "@nextui-org/input";
+import {DatePicker} from "@nextui-org/date-picker";
+import type { CalendarDate } from "@internationalized/date"
+import {parseDate} from "@internationalized/date";
 import {Button} from "@nextui-org/button";
 import React, {  useState } from 'react';
 export default function Panel() {
+  const [addTaskDate, setAddTaskDate] = useState(new Date(new Date().getTime() + 24*60*60*1000)) // 24 hours later
+
+  const dateString = [addTaskDate.getFullYear(),addTaskDate.getMonth() + 1,addTaskDate.getDate()].join('-').toString()
+  const valueDate = parseDate(dateString)
+
   const dispatch = useDispatch()
   const [title,updateTitle] = useState('')
   const [desc,updateDesc] = useState('')
@@ -13,6 +21,7 @@ export default function Panel() {
   function handleDescChange(e: React.FormEvent<HTMLInputElement>) {
     updateDesc(e.currentTarget.value)
   }
+
   function handleAdd(plannedDate?: Date) {
     if (!plannedDate) plannedDate = new Date(new Date().getTime() + 24*60*60*1000) // 24 hours later
     if (title.length < 3) return alert('Title must be at least 3 characters long')
@@ -21,12 +30,22 @@ export default function Panel() {
     updateTitle('')
     updateDesc('')
   }
+
+  function onPickerChange(value: CalendarDate | null) {
+    if (!value) return; 
+    const date = new Date(value.year,value.month-1,value.day)
+    setAddTaskDate(date)
+  }
+
   return <div className="w-[30%] h-full flex justify-center">
     <div className="w-[90%] p-4 rounded-3xl bg-elementBg flex flex-col gap-3">
       <Input label="Title" onChange={handleTitleChange} isRequired value={title} placeholder="Enter task title" variant='faded'/>
-      <Textarea label="Description" onChange={handleDescChange} maxRows={3} variant='faded'/>
-      <Button color="secondary" onPress={() => {handleAdd()}}>Add Tommorow</Button>
-      <Button color="secondary" onPress={() => {handleAdd(new Date())}}>Add Today</Button>
+      <Textarea label="Description" onChange={handleDescChange} value={desc} maxRows={3} variant='faded'/>
+      <div className='flex gap-4'>
+        <Button color="secondary" onPress={() => {handleAdd()}}>Add Tommorow</Button>
+        <div><DatePicker variant='faded' disableAnimation value={valueDate} onChange={onPickerChange}/></div>
+        <Button color="warning" onPress={() => {handleAdd(addTaskDate)}}>Add</Button>
+      </div>
     </div>
   </div>
 }
