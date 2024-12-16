fd8b533cbaf49af38763dbe59dab0c1c3f2aef1b
Hacki <69818162+Hacki01@users.noreply.github.com>
2024-12-16 14:54:08 +0100

    completedTask dropdownmenu

--- src/components/tasks/completedTask.tsx
+++ src/components/tasks/completedTask.tsx
@@ -1,70 +1,56 @@
-import { setCompleted, Task } from "@/TasksStore/Features/tasks/taskManager"
-import { faCaretDown, faCaretRight, faCheck, faExclamation } from "@fortawesome/free-solid-svg-icons"
+import { removeTask, setCompleted, Task } from "@/TasksStore/Features/tasks/taskManager"
+import { faCheck, faCircleExclamation, faEllipsis, faReply, faTrash } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import { useEffect, useRef, useState } from "react"
+import { Button, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Tooltip } from "@nextui-org/react"
+import { Key } from "react"
 import { useDispatch } from "react-redux"
 
 export default function CompletedTask(props:{task:Task}) {
   const task = props.task
+  const id = task.id
   const dispatch = useDispatch()
-  const optionsMenu = useRef<HTMLDivElement>(null)
-  const [isExpanded, setExpanded] = useState(false)
-  
-  useEffect(() => {
-    /* Hide options menu on click outside */
-    const handleClickOutside = (e: MouseEvent) => {
-      if(isExpanded && !optionsMenu.current?.contains(e.target as Node)){
-        setExpanded(false)
-      }
-    }
 
-    document.addEventListener('mousedown', handleClickOutside)
-    return () => {
-      document.removeEventListener('mousedown', handleClickOutside)
+  function doTaskAction(actionName: string | Key) {
+    switch (actionName) {
+      case "setUncompleted":
+        dispatch(setCompleted({id, status: !task.isHighPriority }))
+        break;
+      case "deleteTask":
+        dispatch(removeTask({id}))
+        break;
     }
-  }, [isExpanded])
+  }
 
   return <div className="mb-4 p-6 rounded-2xl border-4 border-green-500 bg-elementBg w-[95%] md:w-[80%] xl:w-[60%] flex items-center gap-4 ">
     {/* Completed mark */}
     <div className='text-3xl'><FontAwesomeIcon icon={faCheck} /></div>
-    {/* Priority mark */}
+    {/* Priority Mark */}
     {task.isHighPriority 
-    ? <div className='text-3xl'><FontAwesomeIcon icon={faExclamation} /></div>
+    ? <Tooltip content="High priority" color='danger'>
+        <FontAwesomeIcon className='text-4xl text-danger' icon={faCircleExclamation} />
+      </Tooltip>
     : null}
     <div className='flex justify-between w-full'>
       <div className='flex flex-col'>
         <div className='text-2xl'>{task.title}</div>
         <div className='text-md'>{task.desc}</div>
       </div> 
-      {/* Is more options visible */}
-      {isExpanded ? (
-        <div 
-          ref={optionsMenu}
-          className="relative select-none p-4 bg-gray-300 hover:bg-gray-400 rounded-2xl aspect-square h-[50px] lg:h-[65px] flex justify-center items-center text-3xl"
-        >
-          <div className='absolute top-0 left-0 p-4 w-auto bg-gray-400 rounded-2xl'>
-            <FontAwesomeIcon icon={faCaretDown} onClick={() => {setExpanded(false)}}/>
-            <div className='flex flex-col gap-1 text-lg text-nowrap'>
-              {/* List of options */}
-              <button 
-                onClick={() => {dispatch(setCompleted({id:task.id, status:false}))}} 
-                className='p-1 rounded-md cursor-pointer hover:bg-gray-300'
-              >
-                Set as Uncompleted
-              </button>
-            </div>
-          </div>
-        </div>
-      ) : (
-        /* Show more options button */
-        <div 
-          title='More options'  
-          onClick={() => {setExpanded(true)}}
-          className="relative p-4 bg-gray-300 hover:bg-gray-400 rounded-2xl aspect-square h-[50px] lg:h-[65px] flex justify-center items-center text-3xl"
-        >
-          <FontAwesomeIcon icon={faCaretRight} />
-        </div>
-      )}
+      <Dropdown>
+            <Tooltip content="More options">
+              <div>
+                <DropdownTrigger>
+                    <Button variant="solid" size="lg" isIconOnly className='text-white text-2xl'><FontAwesomeIcon icon={faEllipsis} /></Button>
+                </DropdownTrigger>
+              </div>
+            </Tooltip>
+          <DropdownMenu onAction={(key) => doTaskAction(key)} aria-label="Static Actions">
+            <DropdownItem key="setUncompleted" startContent={<FontAwesomeIcon icon={faReply}/>}>Set as Uncompleted</DropdownItem>
+            {/* <DropdownItem key="editTask" startContent={"!"}>Edit task</DropdownItem> */}
+            <DropdownItem key="deleteTask" startContent={<FontAwesomeIcon icon={faTrash}/>} className="text-danger" color="danger">
+              Delete task
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
     </div>
   </div>
 }
